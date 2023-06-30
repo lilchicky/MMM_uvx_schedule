68b35d923adba0bd670be93c70d5e8b4b66785b4
lilchicky <thelilchicken01@gmail.com>
2023-06-30 16:54:51 -0600

    simple hello world test

--- uvx_magic_mirror/uvx_schedule.js
+++ uvx_magic_mirror/uvx_schedule.js
@@ -1,1 +1,15 @@
-Module.register("uvx_schedule", {} );
+Module.register("uvx_schedule", {
+
+    defaults: {
+        text: "Hello World!",
+    },
+
+    getDom: function() {
+
+        var wrapper = document.createElement("div");
+        wrapper.innerHTML = this.config.text;
+        return wrapper;
+
+    },
+
+} );
