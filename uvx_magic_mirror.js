17d2059d5211c55fd48bb12b6ad64957aa66c701
lilchicky <thelilchicken01@gmail.com>
2023-07-01 17:00:43 -0600

    test fix?

--- uvx_magic_mirror.js
+++ uvx_magic_mirror.js
@@ -257,7 +257,7 @@ Module.register("uvx_magic_mirror", {
         for(let x = 0; x < satTimes.length; x++) {
           let times = satTimes[x].split(":");
           if (parseInt(times[0]) === hour) {
-            let timeText = this.compareMinutes(minutes, hour, day, times[1], satTimes);
+            let timeText = compareMinutes(minutes, hour, day, times[1], satTimes);
             return timeText;
           }
           else if (x >= satTimes.length - 1) {
@@ -274,7 +274,7 @@ Module.register("uvx_magic_mirror", {
         for(let x = 0; x < weekTimes.length; x++) {
           let times = weekTimes[x].split(":");
           if (parseInt(times[0]) === hour) {
-            let timeText = this.compareMinutes(minutes, hour, day, times[1], weekTimes);
+            let timeText = compareMinutes(minutes, hour, day, times[1], weekTimes);
             return timeText;
           }
           else if (x >= weekTimes.length - 1) {
