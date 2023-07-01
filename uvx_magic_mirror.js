d9b00c7c0aa6e412cfff16d5199e3e12e6999402
lilchicky <thelilchicken01@gmail.com>
2023-06-30 18:36:41 -0600

    attempt to print out stop times

--- uvx_magic_mirror.js
+++ uvx_magic_mirror.js
@@ -7,7 +7,19 @@ Module.register("uvx_magic_mirror", {
     
     getDom: function () {
       var wrapper = document.createElement("div");
+      this.text=getTimesFromFile;
       wrapper.innerHTML = this.config.text;
       return wrapper;
     },
+
+    getTimesFromFile: function() {
+        document.getElementById('2230_north_stops.txt').addEventListener('stops', function() {
+            var fileReader = new FileReader();
+            fileReader.onload=function(){
+                document.getElementById('output').textContent=fileReader.result;
+            }
+            fileReader.readAsText(this.files[0]);
+        })
+    }
+
   });
