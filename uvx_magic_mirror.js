1b257d6020726bc147370aacc50254748af9ebf9
lilchicky <thelilchicken01@gmail.com>
2023-06-30 18:45:25 -0600

    adjustments

--- uvx_magic_mirror.js
+++ uvx_magic_mirror.js
@@ -4,20 +4,19 @@ Module.register("uvx_magic_mirror", {
       text: "Hello World!",
     },
   
-    
-    getDom: function () {
-      var wrapper = document.createElement("div");
-      this.text=getTimesFromFile;
-      wrapper.innerHTML = this.config.text;
-      return wrapper;
-    },
-
     getTimesFromFile: function() {
         const fs = require('fs');
         fs.readFile('2230_north_stops.txt', (err, input) => {
             if (err) throw err;
             return input.toString();
         })
+    },
+
+    getDom: function () {
+      var wrapper = document.createElement("div");
+      this.text=getTimesFromFile();
+      wrapper.innerHTML = this.config.text;
+      return wrapper;
     }
 
   });
