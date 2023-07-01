b3f4dd9478671e1617a3f06c1076019ad1b88ade
lilchicky <thelilchicken01@gmail.com>
2023-06-30 22:00:47 -0600

    Update uvx_magic_mirror.js

--- uvx_magic_mirror.js
+++ uvx_magic_mirror.js
@@ -1,139 +1,140 @@
 Module.register("uvx_magic_mirror", {
     
     defaults: {
-      stopTimes: [
-        "0434",
-        "0504",
-        "0534",
-        "0604",
-        "0634",
-        "0649",
-        "0704",
-        "0716",
-        "0722",
-        "0728",
-        "0734",
-        "0740",
-        "0746",
-        "0752",
-        "0758",
-        "0804",
-        "0810",
-        "0816",
-        "0822",
-        "0828",
-        "0834",
-        "0840",
-        "0846",
-        "0852",
-        "0858",
-        "0904",
-        "0910",
-        "0916",
-        "0922",
-        "0928",
-        "0934",
-        "0940",
-        "0946",
-        "0952",
-        "0958",
-        "1004",
-        "1010",
-        "1016",
-        "1022",
-        "1032",
-        "1042",
-        "1052",
-        "1102",
-        "1112",
-        "1122",
-        "1132",
-        "1142",
-        "1152",
-        "1202",
-        "1212",
-        "1222",
-        "1232",
-        "1242",
-        "1252",
-        "1302",
-        "1312",
-        "1322",
-        "1332",
-        "1342",
-        "1352",
-        "1402",
-        "1412",
-        "1422",
-        "1432",
-        "1442",
-        "1449",
-        "1455",
-        "1401",
-        "1507",
-        "1513",
-        "1519",
-        "1525",
-        "1531",
-        "1537",
-        "1543",
-        "1549",
-        "1555",
-        "1601",
-        "1607",
-        "1613",
-        "1619",
-        "1625",
-        "1631",
-        "1637",
-        "1643",
-        "1649",
-        "1655",
-        "1701",
-        "1707",
-        "1713",
-        "1719",
-        "1725",
-        "1731",
-        "1737",
-        "1743",
-        "1752",
-        "1802",
-        "1812",
-        "1822",
-        "1832",
-        "1842",
-        "1852",
-        "1902",
-        "1912",
-        "1922",
-        "1932",
-        "1942",
-        "1952",
-        "2002",
-        "2012",
-        "2022",
-        "2032",
-        "2042",
-        "2054",
-        "2109",
-        "2124",
-        "2139",
-        "2154",
-        "2209",
-        "2224",
-        "2239",
-        "2259",
-        "2319",
-        "2349",
-        "2319",
-        "2349"
-      ],
 
       "updateInterval" : 1 * 60 * 1000,
       "fadeSpeed": 4000,
       "text": "Start Text"
     },
+
+    stopTimes: [
+      "0434",
+      "0504",
+      "0534",
+      "0604",
+      "0634",
+      "0649",
+      "0704",
+      "0716",
+      "0722",
+      "0728",
+      "0734",
+      "0740",
+      "0746",
+      "0752",
+      "0758",
+      "0804",
+      "0810",
+      "0816",
+      "0822",
+      "0828",
+      "0834",
+      "0840",
+      "0846",
+      "0852",
+      "0858",
+      "0904",
+      "0910",
+      "0916",
+      "0922",
+      "0928",
+      "0934",
+      "0940",
+      "0946",
+      "0952",
+      "0958",
+      "1004",
+      "1010",
+      "1016",
+      "1022",
+      "1032",
+      "1042",
+      "1052",
+      "1102",
+      "1112",
+      "1122",
+      "1132",
+      "1142",
+      "1152",
+      "1202",
+      "1212",
+      "1222",
+      "1232",
+      "1242",
+      "1252",
+      "1302",
+      "1312",
+      "1322",
+      "1332",
+      "1342",
+      "1352",
+      "1402",
+      "1412",
+      "1422",
+      "1432",
+      "1442",
+      "1449",
+      "1455",
+      "1401",
+      "1507",
+      "1513",
+      "1519",
+      "1525",
+      "1531",
+      "1537",
+      "1543",
+      "1549",
+      "1555",
+      "1601",
+      "1607",
+      "1613",
+      "1619",
+      "1625",
+      "1631",
+      "1637",
+      "1643",
+      "1649",
+      "1655",
+      "1701",
+      "1707",
+      "1713",
+      "1719",
+      "1725",
+      "1731",
+      "1737",
+      "1743",
+      "1752",
+      "1802",
+      "1812",
+      "1822",
+      "1832",
+      "1842",
+      "1852",
+      "1902",
+      "1912",
+      "1922",
+      "1932",
+      "1942",
+      "1952",
+      "2002",
+      "2012",
+      "2022",
+      "2032",
+      "2042",
+      "2054",
+      "2109",
+      "2124",
+      "2139",
+      "2154",
+      "2209",
+      "2224",
+      "2239",
+      "2259",
+      "2319",
+      "2349",
+      "2319",
+      "2349"
+    ],
 
     getScripts: function() {
       return ["uvx_magic_mirror.js"];
@@ -143,8 +144,6 @@ Module.register("uvx_magic_mirror", {
       Log.info(`Starting module: ${this.name}`);
   
       this.config.text = "Loading...";
-
-      this.lastComplimentIndex = -1;
   
       // Schedule update timer.
       setInterval(() => {
@@ -152,60 +151,17 @@ Module.register("uvx_magic_mirror", {
       }, this.config.updateInterval);
     },
 
-    randomIndex: function(givenTimes) {
-      if (givenTimes.length === 1) {
-        return 0;
-      }
-      const generate = function() {
-        return Math.floor(Math.random() * givenTimes.length);
-      };
-
-      let timesIndex = generate();
-
-      while (timesIndex === this.lastComplimentIndex) {
-        timesIndex = generate();
-      }
-
-      this.lastComplimentIndex = timesIndex;
-
-      return timesIndex;
-    },
-
-    timeArray: function() {
-      let times = [];
-      const hour = moment().hour();
-      if (hour != 0) {
-        times = this.config.stopTimes;
-      }
-
-      return times;
-    },
-
-    getRandomTime: function() {
-      const times = this.timeArray();
-      let index;
-      index = this.randomIndex(times);
-
-      return times[index] || "";
+    updateDisplay: function() {
+      this.config.text = this.stopTimes[Math.floor(Math.random() * this.stopTimes.length)];
     },
 
     getDom: function() {
         const wrapper = document.createElement("div");
-        const timesText = this.getRandomTime();
-        const parts = timesText.split("\n");
-        const time = document.createElement("span");
-        
-        for(const part of parts) {
-          if (part != "") {
-            time.appendChild(document.createTextNode(part));
-            time.addendChild(docuemnt.createElement("BR"));
-          }
-        }
 
-        if (time.children.length > 0) {
-          time.lastElementChild.remove();
-          wrapper.appendChild(compliment);
-        }
+        let currentText;
+        currentText = updateDisplay();
+
+        wrapper.innerHTML = this.currentText;
         return wrapper;
     }
 
