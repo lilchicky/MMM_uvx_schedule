ad52670ebdfa37319b6df8f496eeda36357d5160
lilchicky <thelilchicken01@gmail.com>
2023-06-30 21:19:27 -0600

    copy paste test

--- uvx_magic_mirror.js
+++ uvx_magic_mirror.js
@@ -139,62 +139,154 @@ Module.register("uvx_magic_mirror", {
       return ["uvx_magic_mirror.js"];
     },
 
-    start: async function() {
-        Log.info('Starting Module: ' + this.name);
-        this.nextStop = "Loading...";
-
-        setInterval(() => {
-          this.updateDom(this.config.fadeSpeed);
-        }, this.config.updateInterval);
+    start: async function () {
+      Log.info(`Starting module: ${this.name}`);
+  
+      this.lastComplimentIndex = -1;
+  
+      if (this.config.remoteFile !== null) {
+        const response = await this.loadComplimentFile();
+        this.config.compliments = JSON.parse(response);
+        this.updateDom();
+      }
+  
+      // Schedule update timer.
+      setInterval(() => {
+        this.updateDom(this.config.fadeSpeed);
+      }, this.config.updateInterval);
     },
-
-    getTime: function () {
-
-      let thisStopTime = [];
-
-      stopTimes.forEach(compareTime)
-
-      let nextStopTime = generate();
-
-      return nextStopTime;
-
+  
+    /**
+     * Generate a random index for a list of compliments.
+     *
+     * @param {string[]} compliments Array with compliments.
+     * @returns {number} a random index of given array
+     */
+    randomIndex: function (compliments) {
+      if (compliments.length === 1) {
+        return 0;
+      }
+  
+      const generate = function () {
+        return Math.floor(Math.random() * compliments.length);
+      };
+  
+      let complimentIndex = generate();
+  
+      while (complimentIndex === this.lastComplimentIndex) {
+        complimentIndex = generate();
+      }
+  
+      this.lastComplimentIndex = complimentIndex;
+  
+      return complimentIndex;
     },
-
-    compareTime: function(stopTime, index) {
+  
+    /**
+     * Retrieve an array of compliments for the time of the day.
+     *
+     * @returns {string[]} array with compliments for the time of the day.
+     */
+    complimentArray: function () {
       const hour = moment().hour();
-      const minute = moment().minute();
-
-      let thisHour = parseInt(stopTime.splice(0,2));
-      let thisMinute = partInt(stoptime.splice(2,3));
-
-      text += index + ": " + item + "\n";
-
+      const date = moment().format("YYYY-MM-DD");
+      let compliments = [];
+  
+      // Add time of day compliments
+      if (hour >= this.config.morningStartTime && hour < this.config.morningEndTime && this.config.compliments.hasOwnProperty("morning")) {
+        compliments = [...this.config.compliments.morning];
+      } else if (hour >= this.config.afternoonStartTime && hour < this.config.afternoonEndTime && this.config.compliments.hasOwnProperty("afternoon")) {
+        compliments = [...this.config.compliments.afternoon];
+      } else if (this.config.compliments.hasOwnProperty("evening")) {
+        compliments = [...this.config.compliments.evening];
+      }
+  
+      // Add compliments based on weather
+      if (this.currentWeatherType in this.config.compliments) {
+        Array.prototype.push.apply(compliments, this.config.compliments[this.currentWeatherType]);
+      }
+  
+      // Add compliments for anytime
+      Array.prototype.push.apply(compliments, this.config.compliments.anytime);
+  
+      // Add compliments for special days
+      for (let entry in this.config.compliments) {
+        if (new RegExp(entry).test(date)) {
+          Array.prototype.push.apply(compliments, this.config.compliments[entry]);
+        }
+      }
+  
+      return compliments;
     },
-
+  
+    /**
+     * Retrieve a file from the local filesystem
+     *
+     * @returns {Promise} Resolved when the file is loaded
+     */
+    loadComplimentFile: async function () {
+      const isRemote = this.config.remoteFile.indexOf("http://") === 0 || this.config.remoteFile.indexOf("https://") === 0,
+        url = isRemote ? this.config.remoteFile : this.file(this.config.remoteFile);
+      const response = await fetch(url);
+      return await response.text();
+    },
+  
+    /**
+     * Retrieve a random compliment.
+     *
+     * @returns {string} a compliment
+     */
+    getRandomCompliment: function () {
+      // get the current time of day compliments list
+      const compliments = this.complimentArray();
+      // variable for index to next message to display
+      let index;
+      // are we randomizing
+      if (this.config.random) {
+        // yes
+        index = this.randomIndex(compliments);
+      } else {
+        // no, sequential
+        // if doing sequential, don't fall off the end
+        index = this.lastIndexUsed >= compliments.length - 1 ? 0 : ++this.lastIndexUsed;
+      }
+  
+      return compliments[index] || "";
+    },
+  
+    // Override dom generator.
     getDom: function () {
-      var wrapper = document.createElement("div");
-
-      const stopTime = this.getTime();
-
-      const parts = text.split("\n");
-
-		  const times = document.createElement("span");
-
-		  for (const part of parts) {
-			  if (part !== "") {
-
-			  	times.appendChild(document.createTextNode(part));
-
-				  times.appendChild(document.createElement("BR"));
-			  }
-		  }
-
-		  if (times.children.length > 0) {
-
-		  	times.lastElementChild.remove();
-		  	wrapper.appendChild(times);
-		  }
+      const wrapper = document.createElement("div");
+      wrapper.className = this.config.classes ? this.config.classes : "thin xlarge bright pre-line";
+      // get the compliment text
+      const complimentText = this.getRandomCompliment();
+      // split it into parts on newline text
+      const parts = complimentText.split("\n");
+      // create a span to hold the compliment
+      const compliment = document.createElement("span");
+      // process all the parts of the compliment text
+      for (const part of parts) {
+        if (part !== "") {
+          // create a text element for each part
+          compliment.appendChild(document.createTextNode(part));
+          // add a break
+          compliment.appendChild(document.createElement("BR"));
+        }
+      }
+      // only add compliment to wrapper if there is actual text in there
+      if (compliment.children.length > 0) {
+        // remove the last break
+        compliment.lastElementChild.remove();
+        wrapper.appendChild(compliment);
+      }
       return wrapper;
+    },
+  
+    // Override notification handler.
+    notificationReceived: function (notification, payload, sender) {
+      if (notification === "CURRENTWEATHER_TYPE") {
+        this.currentWeatherType = payload.type;
+      }
     }
 
   });
